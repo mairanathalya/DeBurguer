5902f7e0429fb688affce029005c8293559d78d7
mairanathalya <maira.nathalya@gmail.com>
2023-09-06 14:06:53 -0300

    Initial commit

---
+++ src/main/java/com/mairanath/deburguer/DeburguerApplication.java
@@ -0,0 +1,13 @@
+package com.mairanath.deburguer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeburguerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeburguerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mairanath/deburguer/DeburguerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mairanath.deburguer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeburguerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
